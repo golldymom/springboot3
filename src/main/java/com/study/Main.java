e10013c3fc38787f1b9e7013fd485c7c6947a4ca
남경화 <khnam5@naver.com>
2023-01-20 12:09:11 +0900

    first

---
+++ src/main/java/com/study/Main.java
@@ -0,0 +1,81 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
+
+@SpringBootApplication // 컴포넌트스캔과 인에이블오토컨피궐레이션 대신 쓸수 있음 스프링부트
+//@ComponentScan(basePackages = "com.study") 아래이 3개는 스프링 어노테이션
+//@EnableAutoConfiguration
+//@Configuration
+@RestController
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello!");
+        SpringApplication.run(Main.class, args);
+    }
+//@GetMapping("/greet")
+//    public String greet() {
+//        return "Hello";
+//    }
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+              GreetResponse response= new GreetResponse(
+                      "Hello",
+                      List.of("Java", "Golang"),
+                      new Person("Alex", 28, 30)
+               );
+        return response;
+    }
+
+    record Person(String name, int age, double savings) {
+
+    }
+        record GreetResponse(
+                String greet,
+                List<String> favProgrammingLanguages,
+                Person person
+        ) {
+    }
+    //json으로 받는 방법 아래 쭉
+//    class GreetResponse {
+//        private final String greet;
+//
+//        GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "GreetResponse{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            GreetResponse that = (GreetResponse) o;
+//            return greet.equals(that.greet);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(greet);
+//        }
+//    }
+
+}
